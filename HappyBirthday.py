d53f206a7b8570d213792292154eadec5287c1e0
Kazi Mahbubur Rahman <63844626+NoobMahbub@users.noreply.github.com>
2022-01-20 14:11:15 +0600

    Create HappyBirthday.py

---
+++ HappyBirthday.py
@@ -0,0 +1,431 @@
+#Run the code ^_^
+import turtle
+from datetime import date
+
+
+Today = date.today() #getting today's date
+Birthday = date(Today.year,1,20) 
+#getting current year so that this code works in your every birthday :D 
+
+
+#if today is your birthday it will wish you by using python turtle...
+if (Today == Birthday):
+    print("Happy Birthday 01001010 01100001 01100001 01101110 01100001 it's",Birthday) 
+    #using binary because I don't want to show this message to your friends shorom lage ._.
+
+    birthday=turtle.Turtle()
+    birthday.width(8)
+    birthday.color("#00FF00")
+    new=turtle.getscreen()
+    birthday.speed(10)
+
+    new.bgcolor("lightblue")
+
+    # Hidden Work(penup)
+    birthday.left(180)
+    birthday.penup()
+    birthday.forward(300)
+    birthday.right(90)
+    birthday.forward(100)
+    birthday.pendown()
+
+    # Printing H
+
+    #start to draw
+    birthday.forward(50)
+    birthday.right(90)
+    birthday.forward(50)
+    birthday.left(90)
+    birthday.forward(50)
+    birthday.left(90)
+
+    birthday.penup()
+    birthday.forward(50)
+    birthday.left(90)
+    birthday.pendown()
+    birthday.forward(50)
+    birthday.left(90)
+    birthday.forward(50)
+    birthday.right(90)
+    birthday.forward(50)
+
+
+    # printing A
+
+    birthday.penup()
+    birthday.left(90)
+    birthday.forward(15)
+    birthday.pendown()
+    birthday.left(70)
+    birthday.forward(110)
+    birthday.right(70)
+    birthday.right(70)
+    birthday.forward(110)
+    birthday.left(180)
+    birthday.forward(55)
+    birthday.left(70)
+    birthday.forward(38)
+    birthday.left(70)
+    birthday.penup()
+    birthday.forward(55)
+    birthday.left(110)
+
+    birthday.forward(100)
+
+    # printing P
+
+    birthday.left(90)
+    birthday.pendown()
+    birthday.forward(100)
+    birthday.right(90)
+    birthday.forward(50)
+    birthday.right(20)
+    birthday.forward(20)
+    birthday.right(70)
+    birthday.forward(40)
+    birthday.right(70)
+    birthday.forward(20)
+    birthday.right(20)
+    birthday.forward(50)
+    birthday.left(90)
+    birthday.forward(50)
+    birthday.left(90)
+    birthday.penup()
+    birthday.forward(100)
+
+
+    # printing P
+
+    birthday.left(90)
+    birthday.pendown()
+    birthday.forward(100)
+    birthday.right(90)
+    birthday.forward(50)
+    birthday.right(20)
+    birthday.forward(20)
+    birthday.right(70)
+    birthday.forward(40)
+    birthday.right(70)
+    birthday.forward(20)
+    birthday.right(20)
+    birthday.forward(50)
+    birthday.left(90)
+    birthday.forward(50)
+    birthday.left(90)
+    birthday.penup()
+    birthday.forward(100)
+
+    # printing Y
+
+    birthday.forward(20)
+    birthday.pendown()
+    birthday.left(90)
+    birthday.forward(50)
+    birthday.left(30)
+    birthday.forward(60)
+    birthday.backward(60)
+    birthday.right(60)
+    birthday.forward(60)
+    birthday.backward(60)
+    birthday.left(30)
+
+    # go to Home
+
+    birthday.penup()
+    birthday.home()
+
+    birthday.color("orange")
+    new.bgcolor("white")
+    # setting second row
+
+    birthday.backward(300)
+    birthday.right(90)
+    birthday.forward(60)
+    birthday.left(180)
+
+
+    # printing P
+
+
+    birthday.pendown()
+    birthday.forward(100)
+    birthday.right(90)
+    birthday.forward(50)
+    birthday.right(20)
+    birthday.forward(20)
+    birthday.right(70)
+    birthday.forward(40)
+    birthday.right(70)
+    birthday.forward(20)
+    birthday.right(20)
+    birthday.forward(50)
+    birthday.backward(50)
+    birthday.left(180)
+    birthday.right(20)
+    birthday.forward(20)
+    birthday.right(70)
+    birthday.forward(40)
+    birthday.right(70)
+    birthday.forward(20)
+    birthday.right(20)
+    birthday.forward(50)
+    birthday.right(90)
+    birthday.forward(10)
+
+
+    # go to Home
+
+    birthday.penup()
+    birthday.home()
+
+    # setting up
+
+    birthday.backward(200)
+    birthday.right(90)
+    birthday.forward(10)
+    birthday.left(90)
+    birthday.pendown()
+    birthday.forward(20)
+    birthday.penup()
+    birthday.home()
+
+    # D
+
+    birthday.backward(150)
+    birthday.right(90)
+    birthday.forward(60)
+    birthday.pendown()
+    birthday.backward(100)
+    birthday.right(90)
+    birthday.forward(10)
+    birthday.backward(70)
+    birthday.left(180)
+    birthday.right(20)
+    birthday.forward(20)
+    birthday.right(70)
+    birthday.forward(88)
+    birthday.right(70)
+    birthday.forward(20)
+    birthday.right(20)
+    birthday.forward(70)
+
+    birthday.penup()
+    birthday.home()
+
+    # set up for A
+
+    birthday.backward(50)
+    birthday.right(90)
+    birthday.forward(65)
+    birthday.left(90)
+
+
+
+    # printing A
+
+
+    birthday.pendown()
+    birthday.left(70)
+    birthday.forward(110)
+    birthday.right(70)
+    birthday.right(70)
+    birthday.forward(110)
+    birthday.left(180)
+    birthday.forward(55)
+    birthday.left(70)
+    birthday.forward(38)
+    birthday.left(70)
+    birthday.penup()
+    birthday.forward(55)
+    birthday.left(110)
+
+    birthday.forward(100)
+
+    # printing Y
+
+
+    birthday.pendown()
+    birthday.left(90)
+    birthday.forward(50)
+    birthday.left(30)
+    birthday.forward(60)
+    birthday.backward(60)
+    birthday.right(60)
+    birthday.forward(60)
+    birthday.backward(60)
+    birthday.left(30)
+
+    # go to Home
+
+    birthday.penup()
+    birthday.home()
+
+
+    # settig pogition
+
+    birthday.right(90)
+    birthday.forward(215)
+    birthday.right(90)
+    birthday.forward(200)
+    birthday.right(90)
+
+    #color
+
+    birthday.color("blue")
+    new.bgcolor("black")
+
+    #printing Somiya
+
+
+
+    birthday.pendown()
+    # printing S
+    birthday.color("black")
+    birthday.right(90)
+    birthday.forward(0)
+    birthday.color("red")
+    birthday.forward(50)
+    birthday.back(50)
+    birthday.right(90)
+    birthday.forward(50)
+    birthday.left(90)
+    birthday.forward(50)
+    birthday.right(90)
+    birthday.forward(50)
+    birthday.right(90)
+    birthday.forward(50)
+    birthday.right(180)
+    birthday.forward(50)
+
+    # o
+
+    birthday.color("black")
+    birthday.forward(50)
+    birthday.color("red")
+    birthday.forward(50)
+    birthday.back(50)
+    birthday.left(90)
+    birthday.forward(100)
+    birthday.right(90)
+    birthday.forward(50)
+    birthday.right(90)
+    birthday.forward(100)
+    birthday.left(90)
+    # M
+    birthday.color("black")
+    birthday.forward(90)
+    birthday.color("red")
+    birthday.left(120)
+    birthday.forward(120)
+    birthday.right(210)
+    birthday.forward(100)
+    birthday.back(100)
+    birthday.left(30)
+    birthday.forward(100)
+    birthday.left(120)
+    birthday.forward(100)
+    birthday.left(210)
+    birthday.forward(100)
+    birthday.back(100)
+
+    # I
+    birthday.color("black")
+    birthday.left(90)
+    birthday.forward(30)
+    birthday.color("red")
+    birthday.forward(30)
+    birthday.back(15)
+    birthday.right(90)
+    birthday.forward(100)
+    birthday.right(90)
+    birthday.forward(15)
+    birthday.back(15)
+    birthday.right(180)
+    birthday.forward(15)
+    # Y
+    birthday.color("black")
+    birthday.forward(80)
+    birthday.color("red")
+    birthday.left(90)
+    birthday.forward(50)
+    birthday.left(45)
+    birthday.forward(75)
+    birthday.back(75)
+    birthday.right(90)
+    birthday.forward(75)
+    birthday.back(75)
+    birthday.left(45)
+    birthday.back(50)
+    birthday.right(90)
+
+    #A
+    birthday.color("black")
+    birthday.forward(80)
+    birthday.left(60)
+    birthday.color("red")
+    birthday.forward(110)
+    birthday.right(120)
+    birthday.forward(110)
+    birthday.back(55)
+    birthday.right(125)
+    birthday.forward(50)
+    birthday.back(50)
+    #A end
+
+    # setup
+    birthday.penup()
+    birthday.left(90)
+    birthday.forward(80)
+    birthday.left(90)
+    birthday.forward(7)
+
+
+    birthday.forward(100)
+
+    # design
+
+
+    #design pattern
+    birthday.home()
+    birthday.forward(200)
+    birthday.pendown()
+    birthday.color("PURPLE")
+    birthday.width(3)
+    birthday.speed(0)
+
+    def squre(length, angle):
+
+        birthday.forward(length)
+        birthday.right(angle)
+        birthday.forward(length)
+        birthday.right(angle)
+
+        birthday.forward(length)
+        birthday.right(angle)
+        birthday.forward(length)
+        birthday.right(angle)
+
+    squre(80, 90)
+
+    for i in range(36):
+        birthday.right(10)
+        squre(80, 90)
+
+#Heart shape on cake thanks to Jhankar dadu :p
+
+    birthday.begin_fill()
+    birthday.color("red")
+    birthday.left(50)
+    birthday.forward(100)
+    birthday.circle(40, 180)
+    birthday.left(260)
+    birthday.circle(40, 180)
+    birthday.forward(100)
+    birthday.end_fill()
+
+
+    turtle.mainloop()
+
+else:
+    print("Oops looks like it's not your birthday today, I will only wish in your birthday")
+#Made with Love by MahbubDev
